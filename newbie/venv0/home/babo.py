b23c07a6acb2da4e9424a7b49e69b069cfdde7d6
hwanghb <redhhb0@gmail.com>
2024-04-16 09:33:40 +0900

    456

--- newbie/venv0/home/babo.py
+++ newbie/venv0/home/babo.py
@@ -2,16 +2,16 @@ import random
 
 player_input = input("할거임? (y or n)")
 def randnum():
-    if player_input == y:
+    if player_input == "y":
         bebe = random.randint(0,98)
         return bebe
-    elif player_input == n:
+    elif player_input == "n":
         bebe = "잘가요"
         return bebe
     else:
         bebe = "넌 뭐야"
         return bebe
-result = player_input()
+result = randnum()
 print('결과 : ', result)
 
 if result == 7:
