3dd1d800240a3c172b4581e928c446b73e383a0e
hwanghb <redhhb0@gmail.com>
2024-04-17 09:18:02 +0900

    hhb

--- Beeeeen/gpt/gptarrange.py
+++ Beeeeen/gpt/gptarrange.py
@@ -10,7 +10,18 @@ print("가장 큰 수는:", max_number)
 numbers = [1, 2, 2, 3, 4, 4, 5]
 unique_numbers = list(set(numbers))
 print("중복을 제거한 숫자들:", unique_numbers)
-'''
+
 number = [1, 2, 3, 4, 4, 3, 2, 1]
 sortednumber = sorted(number)
-print(sortednumber, '\n', number.sort() )
+number.sort()
+print(sortednumber, '\n', number)
+
+numbers = [1,2,3,4,5,6]
+reversed = numbers[::-1]
+print(reversed)
+
+num1 = [1,2,3]
+num2 = [4,5,6]
+merged = num1 + num2
+print(merged)
+'''
