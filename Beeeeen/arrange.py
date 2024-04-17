52148274351bb03d0f5e019327702ecf77277e9d
hwanghb <redhhb0@gmail.com>
2024-04-17 10:34:30 +0900

    hhb

---
+++ Beeeeen/arrange.py
@@ -0,0 +1,41 @@
+# number = "1, 2, 3, 4"
+# number1 = number.split(',')
+# print(number1)
+
+# numbers = [5, 3, 8, 2, 9, 1]
+# max_number = max(numbers)
+# print("가장 큰 수는:", max_number)
+
+# numbers = [1, 2, 2, 3, 4, 4, 5]
+# unique_numbers = list(set(numbers))
+# print("중복을 제거한 숫자들:", unique_numbers)
+
+# number = [1, 2, 3, 4, 4, 3, 2, 1]
+# sortednumber = sorted(number)
+# number.sort()
+# print(sortednumber, '\n', number)
+
+# numbers = [1,2,3,4,5,6]
+# reversed = numbers[::-1]
+# print(reversed)
+
+# num1 = [1,2,3]
+# num2 = [4,5,6]
+# merged = num1 + num2
+# print(merged)
+
+# import os
+
+# # 현재 작업 디렉토리를 확인
+# current_directory = os.getcwd()
+# print("현재 작업 디렉토리:", current_directory)
+
+# import os
+
+# pathreal = os.path.exists(r"C:\Users\windows\Desktop\aaesongie\Beeeeen\gpt\gptdiary.py")
+# print(pathreal)
+
+import os
+
+datalist = os.listdir()
+print(datalist)
