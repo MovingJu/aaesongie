e315e8abea7ebd98356737eacd9f4a22693514dd
hwanghb <redhhb0@gmail.com>
2024-04-17 14:21:26 +0900

    hhb

--- Beeeeen/doitkim.py
+++ Beeeeen/doitkim.py
@@ -10,4 +10,16 @@
 # print(round(num, ndigits=None))
 # print(round(num, -1))
 # print(round(num, -2))
+# round(num, 1)
+
+# func = lambda a,b : a+b
+# print(func(1,2))
+
+fefe = lambda a : a[:11]
+senten = '''달디달고 달디달고 달디단 밤양갱 따라라라란 떠나는 길에 네가 내게 말했지 너는 바라는게 너무나 많아'''
+# print(senten[:10])
+print(fefe(senten))
+
+# regeno = [1,2,3,4,5,6,7,8,9,10]
+# print(regeno[1:3])
 
