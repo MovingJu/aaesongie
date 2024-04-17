9c64924095828b85b63f7db43f2891eb40a6c463
hwanghb <redhhb0@gmail.com>
2024-04-17 13:25:18 +0900

    hhb

--- Beeeeen/updown.py
+++ Beeeeen/updown.py
@@ -1,14 +1,15 @@
 import random as rd
 import time
 
-rdnumber = rd.randint(0,9999)
+lim = int(input("최대 숫자를 입력하세요 : "))
+rdnumber = rd.randint(0,lim)
 
 def travel(x):
     time.sleep(0.5)
     calcul(x)
 
 def calcul(x):
-    choice = int(input('\n9999 이하의 자연수를 제시해주세요 : '))
+    choice = int(input(f'\n{lim} 이하의 자연수를 제시해주세요 : '))
 
     if choice == rdnumber:
         print('\n정답!')
@@ -17,7 +18,7 @@ def calcul(x):
             print('한번만에 맞추셨어요! 놀라운걸요?')
         else:
             print(f'총 {x}번만에 맞추셨어요!')
-    elif 0 <= choice <= 9999:
+    elif 0 <= choice <= lim:
         if choice < rdnumber:
             print('\n업')
             x = x + 1
