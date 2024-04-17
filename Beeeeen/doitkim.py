b12804b1781cfb374ac7d90939d5897d95af08c3
hwanghb <redhhb0@gmail.com>
2024-04-17 14:12:38 +0900

    hhb

---
+++ Beeeeen/doitkim.py
@@ -0,0 +1,13 @@
+# for i in range(1,11):
+#     print(str(i), '번쨰 인사')
+#     print(i, '번쨰 인사')
+
+# num = float(input("숫자 입력 : "))
+# print('%f' %num)
+# print(round(num))
+# print(round(num, 1))
+# print(round(num, 2))
+# print(round(num, ndigits=None))
+# print(round(num, -1))
+# print(round(num, -2))
+
--- Beeeeen/updown.py
+++ Beeeeen/updown.py
@@ -1,9 +1,6 @@
 import random as rd
 import time
 
-lim = int(input("최대 숫자를 입력하세요 : "))
-rdnumber = rd.randint(0,lim)
-
 def travel(x):
     time.sleep(0.5)
     calcul(x)
@@ -12,20 +9,21 @@ def calcul(x):
     choice = int(input(f'\n{lim} 이하의 자연수를 제시해주세요 : '))
 
     if choice == rdnumber:
+        x = x + 1
         print('\n정답!')
         time.sleep(0.5)
-        if x == 0:
-            print('한번만에 맞추셨어요! 놀라운걸요?')
+        if x == 1:
+            print('한번만에 맞추셨어요! 놀라운걸요?\n')
         else:
-            print(f'총 {x}번만에 맞추셨어요!')
-    elif 0 <= choice <= lim:
+            print(f'총 {x}번만에 맞추셨어요!\n')
+    elif 1 <= choice <= lim:
         if choice < rdnumber:
-            print('\n업')
             x = x + 1
+            print('\n업')
             travel(x)
         elif choice > rdnumber:
-            print('\n다운')
             x = x + 1
+            print('\n다운')
             travel(x)
     else:
         print('\n9999 이하의 자연수만 입력해주세요')
@@ -33,4 +31,6 @@ def calcul(x):
 
 if __name__ == '__main__':
     print('\n===== 파이썬 업다운 게임 =====')
+    lim = int(input("\n최대 숫자를 입력하세요 : "))
+    rdnumber = rd.randint(1,lim)
     calcul(0)
